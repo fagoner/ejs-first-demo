fa467b3bb65789eefdf54675990c14f52bfae4be
fagoner <farendaway@gmail.com>
2021-05-27 17:55:29 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# EJS-FIRST-DEMO
+
+A simple demo using `express@4.17.1`, `ejs@3.1.6`
+
+## Run on dev mode with nodemon
+```
+npm install
+npm install -D
+
+npm run dev
+```
+
+## Run without nodemon
+```npm server.js```
+
+or...
+
+```npm run start```
+
+## Main file
+```server.js```
+
+## Credits
+- [do-community/ejs-demo](https://github.com/do-community/ejs-demo)
+
+## Result
+
+![demo](_readme/demo.png)
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', 3000);
+
+app.get('/', function (req, resp) {
+    const title = "Favorites Albums";
+
+    const albums = [
+        { title: 'Ten', author: 'Pearl Jam' },
+        { title: 'Audioslave', author: 'Audioslave' },
+        { title: 'Justice for all', author: 'Metallica' },
+        { title: 'Naugthy By Nature', author: 'Naughty By Nature' },
+        { title: 'Dangerous', author: 'Michael Jackson' }
+    ];
+
+
+    resp.render('index', { title, albums: albums })
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Listening on port: ', app.get('port'));
+})
+console.log('hi');
